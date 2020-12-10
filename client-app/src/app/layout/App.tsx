9112091906957a1a2664cd5e5ae98b426d83e25e
Mihailo Zivkovic <jevgenije@hotmail.com>
2020-12-10 19:08:37 +0100

    EndOfSection9

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -5,7 +5,7 @@ import  Navbar  from '../../features/nav/Navbar';
 import  ActivityDashboard  from '../../features/activities/dashboard/ActivityDashboard';
 import { observer } from 'mobx-react-lite';
 import { Route, RouteComponentProps, withRouter } from 'react-router-dom';
-import { HomePage } from '../../features/home/HomePage';
+import HomePage  from '../../features/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import  ActivityDetails  from '../../features/activities/details/ActivityDetails';
 
