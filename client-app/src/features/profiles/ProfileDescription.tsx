029c3a5703b082a10638e74cdcf7a2f92c0fc64b
Mihailo Zivkovic <jevgenije@hotmail.com>
2021-03-13 10:22:07 +0100

    end of section 19

---
+++ client-app/src/features/profiles/ProfileDescription.tsx
@@ -0,0 +1,45 @@
+import { profile } from 'console'
+import { observer } from 'mobx-react-lite'
+import React, { useContext, useState } from 'react'
+import { Button, Grid, GridColumn, Header, Tab } from 'semantic-ui-react'
+import { RootStoreContext } from '../../app/stores/rootStore'
+import ProfileEditForm from './ProfileEditForm'
+
+
+ const ProfileDescription = () => {
+     const rootStore = useContext(RootStoreContext);
+     const {updateProfile, profile, isCurrentUser } = rootStore.profileStore;
+     const [editMode, setEditMode] = useState(false);
+    return (
+        <Tab.Pane>
+            <Grid>
+                <GridColumn width={16}>
+                    <Header
+                    floated='left'
+                    icon='user'
+                    content={`About ${profile!.username}`}
+                    />
+                {isCurrentUser && (
+                    <Button
+                    floated='right'
+                    basic
+                    content={editMode ? 'Cancel' : 'Edit Profile'}
+                    onClick={() => setEditMode(!editMode)}
+                    />
+                )}
+                </GridColumn>
+                <GridColumn width={16}>
+                    {editMode ? (
+                        <ProfileEditForm
+                        updateProfile={updateProfile}
+                        profile = {profile!}
+                        />
+                    ) : ( <span>{profile!.bio}</span>
+                    )}
+                </GridColumn>
+            </Grid>
+        </Tab.Pane>
+    );
+}
+
+export default observer(ProfileDescription)
